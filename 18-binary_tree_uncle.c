e877086cc07146395da2f8968155883b09c80d6e
olayyaMansour <mansour.olayya@gmail.com>
2024-03-28 17:54:37 -0700

    add task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,5 +13,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	return (binary_tree_sibling(node->parent));
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	
+	return (node->parent->parent->left);
 }
