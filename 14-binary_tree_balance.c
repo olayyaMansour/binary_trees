7fe9dcfd1b79ea58619717c20dd541719af92cc3
olayyaMansour <mansour.olayya@gmail.com>
2024-03-28 17:47:19 -0700

    fix buf

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,13 +8,27 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-	if (tree == NULL)
-		return (0);
+	return (0);
+}
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+/**
+ * binary_tree_height - calculates the height of a binary tree.
+ * @tree: A pointer.
+ *
+ * Return: return height or null if tree is null.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t left_height = 0, right_height = 0;
 
-	return (left_height - right_height);
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((left_height > right_height) ? left_height : right_height);
+	}
+	return (0);
 }
