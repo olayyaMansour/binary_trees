95db685adcf5973f36870b6c50d1c08775c72aaf
olayyaMansour <mansour.olayya@gmail.com>
2024-03-28 17:56:17 -0700

    fix betty style

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -15,6 +15,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
-	
+
 	return (node->parent->parent->left);
 }
